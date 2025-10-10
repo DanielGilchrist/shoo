0c149725b9b614cd9189e7da29c4ec34b2719a9a
Daniel Gilchrist <daniel@workforce.com>
2025-10-10 11:02:38 +0100

    Add the rest of the possible notification reasons
    https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28&versionId=free-pro-team%40latest&productId=rest

--- src/shoo/api/notification_reason.cr
+++ src/shoo/api/notification_reason.cr
@@ -1,13 +1,17 @@
 module Shoo
   module API
     enum NotificationReason
+      ApprovalRequested
       Assign
       Author
       CiActivity
       Comment
+      Invitation
       Manual
+      MemberFeatureRequested
       Mention
       ReviewRequested
+      SecurityAdvisoryCredit
       SecurityAlert
       StateChange
       Subscribed
