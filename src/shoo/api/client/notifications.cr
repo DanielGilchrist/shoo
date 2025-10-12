9b7c47cfc7f345187fda896aca3f7028dfe63349
Daniel Gilchrist <danniieelg@gmail.com>
2025-10-12 02:35:33 +0100

    Paginate notifications

--- src/shoo/api/client/notifications.cr
+++ src/shoo/api/client/notifications.cr
@@ -5,16 +5,20 @@ module Shoo
         def initialize(@client : Client)
         end
 
-        def list(per_page : UInt8) : API::Result(Array(Notification))
+        # https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28&versionId=free-pro-team%40latest&productId=rest#list-notifications-for-the-authenticated-user
+        def list(page : Int32 = 1, per_page : Int32 = 50) : API::Result(Array(Notification))
           @client.get(Array(Notification), "/notifications", query: {
+            "page"     => page.to_s,
             "per_page" => per_page.to_s,
           })
         end
 
+        # https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28&versionId=free-pro-team%40latest&productId=rest#mark-notifications-as-read
         def mark_as_done(notification_id : String) : Bool
           @client.delete("/notifications/threads/#{notification_id}")
         end
 
+        # https://docs.github.com/en/rest/activity/notifications?apiVersion=2022-11-28#delete-a-thread-subscription
         def unsubscribe(notification_id : String) : Bool
           @client.delete("/notifications/threads/#{notification_id}/subscription")
         end
