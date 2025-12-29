28177252f0a281d58b22370c8ceb0e6927ae9ca2
Daniel Gilchrist <danniieelg@gmail.com>
2025-12-29 20:51:13 +0000

    Avoid allocating headers on each API call

--- src/shoo/api/client/request.cr
+++ src/shoo/api/client/request.cr
@@ -2,16 +2,20 @@ module Shoo
   module API
     struct Client
       struct Request
+        @headers : HTTP::Headers
+
         alias TBody = Hash(String, String)
 
         HTTPS       = "https"
         GITHUB_HOST = "api.github.com"
         BASE_URL    = "#{HTTPS}://#{GITHUB_HOST}"
 
-        def initialize(@token : String); end
+        def initialize(token : String)
+          @headers = build_headers(token)
+        end
 
         def get(type : T.class, path : String, query : TBody = TBody.new) : API::Result(T) forall T
-          response = HTTP::Client.get(build_uri(path, query), headers: headers)
+          response = HTTP::Client.get(build_uri(path, query), headers: @headers)
           API::Result(T).from(response)
         end
 
@@ -21,20 +25,20 @@ module Shoo
         end
 
         def delete(path : String) : Bool
-          response = HTTP::Client.delete("#{BASE_URL}#{path}", headers: headers)
+          response = HTTP::Client.delete("#{BASE_URL}#{path}", headers: @headers)
           response.success?
-        end
-
-        private def headers
-          HTTP::Headers{
-            "Authorization"        => "Bearer #{@token}",
-            "X-GitHub-Api-Version" => "2022-11-28",
-          }
         end
 
         private def build_uri(path : String, query : TBody = TBody.new) : URI
           params = URI::Params.encode(query)
           URI.new(HTTPS, GITHUB_HOST, path: path, query: params)
+        end
+
+        private def build_headers(token : String) : HTTP::Headers
+          HTTP::Headers{
+            "Authorization"        => "Bearer #{token}",
+            "X-GitHub-Api-Version" => "2022-11-28",
+          }
         end
       end
     end
